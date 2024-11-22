def6e775d0ea978de80a61b5c915ac4304de55ef
nagxsan <sanchetng@gmail.com>
2024-11-22 17:23:02 +0530

    minor change

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const userModel = require('./models')
+const { userModel } = require('./models')
 
 const app = express();
 
